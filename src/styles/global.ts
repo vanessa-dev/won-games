1c474dd2713a55ce77c58328ba5200eb3af97ff9
Vanessa Santana <vanessa_santanatoledo@outlook.com>
2022-10-31 16:01:48 -0300

    class(modulo3): Configurando Theme Provider e suporte do TypeScript with declaration file

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
   @font-face {
@@ -36,17 +36,15 @@ const GlobalStyles = createGlobalStyle`
 
   }
 
-  html {
-    font-size: 62.5%;
-  }
+  ${({ theme }) => css`
+    html {
+      font-size: ${theme.font.sizes.medium};
+    }
 
-  html, body, #__next {
-    height: 100%;
-  }
-
-  body {
-    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-  }
+    body {
+      font-family: ${theme.font.family};
+    }
+  `}
 `
 
 export default GlobalStyles
---
+++ styled-components.d.ts
@@ -0,0 +1,6 @@
+import theme from 'styles/theme'
+type Theme = typeof theme
+declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {}
+}
